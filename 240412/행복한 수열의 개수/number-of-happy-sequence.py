164b231a5f9858a97939957bfe236f3d0ba85487
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2024-04-12 11:19:58 +0900

    Title: 행복한 수열의 개수, Time: 146ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240412/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,42 @@
+'''
+행 기준으로만 완탐하면 될듯
+
+'''
+
+# 수열을 합쳐서 정렬 시키면 -> m개씩 맞아 떨어지는지 확인
+from copy import deepcopy
+n, m = map(int, input().split())
+arr = []
+for _ in range(n):
+    arr.append(list(map(int, input().split())))
+
+ans = 0
+# 먼저 행검사 
+for i in range(n):
+    temp = arr[i]
+    pre = temp[0]
+    cnt = 1
+    for j in range(1, n):
+        if pre == temp[j]:
+            cnt += 1
+        pre = temp[j]
+
+    if cnt >= m:
+        ans += 1
+
+for i in range(n):
+    temp = []
+    for j in range(n):
+        temp.append(arr[i][j])
+        
+    pre = temp[0]
+    cnt = 1
+    for j in range(1, n):
+        if pre == temp[j]:
+            cnt += 1
+        pre = temp[j]
+        
+    if cnt >= m:
+        ans += 1
+    
+print(ans)
