f5f00c8327ba90a41ec970c49e7557d7e6c03edd
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2024-04-13 22:33:42 +0900

    Title: 비를 피하기, Time: 243ms, Memory: 26MB, Status: Passed - CodeTree

--- 240413/비를 피하기/stay-out-of-rain.py
+++ 240413/비를 피하기/stay-out-of-rain.py
@@ -1,15 +1,14 @@
 '''
-n * n
-h명
+10000개를 기준으로 4방향으로 한번 
+최대 돌려봤자 10000번 찍는 것
+10 ** 8
 
-가장 가까운 곳까지의 거리
-0으론 이동 가능 1은 벽, 2까지 가능
-3이 도착지
-상하좌우
+원래는 10000개를 기준으로 10000개의 출구를 기준으로 매번 배열 10000개를 다 돌면서 찍는 것
+10000 ** 3
 
-
+=> 이럴땐 관점을 바꿔서, 출구를 기준으로 각 칸에 대한 최소 점을 구한다. 
 '''
-import heapq
+from collections import deque
 INF = int(1e9)
 
 n, h, m = map(int, input().split())
@@ -25,51 +24,34 @@ for i in range(n):
             goals.append((i, j))
 
 # check
-def check(sx, sy):
-    ans = []
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
+# 칸이 1이면 벽 
+# 가중치가 다 똑같으므로 deque도 가능해 
 
-    for k in range(m):
-        ex, ey = goals[k]
-        q = []
-        heapq.heappush(q, (0, sx, sy))
-        visited = [[False] * n for _ in range(n)]
-        visited[sx][sy] = True
-        distance = [[INF] * n for _ in range(n)]
-        distance[sx][sy] = 0
+q = deque([])
+for i in range(m):
+    x, y = goals[i]
+    q.append((x, y, 0))
 
-        while q:
-            cnt, x, y = heapq.heappop(q)
-            # 넘어가면 안되고, 1이면 안되고, 2/0 이면 통과 가능
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-            if cnt > distance[x][y]:
-                continue
+step = [[INF] * n for _ in range(n)]
 
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
+while q:
+    x, y, cnt = q.popleft()
 
-                if nx < 0 or nx >= n or ny < 0 or ny >= n:
-                    continue
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
 
-                if arr[nx][ny] == 1:
-                    continue
-
-                if (nx, ny) == (ex, ey):
-                    ans.append(cnt + 1)
-                    break
-
-                if not visited[nx][ny]:
-                    if cnt + 1 < distance[nx][ny]:
-                        distance[nx][ny] = cnt + 1
-                        heapq.heappush(q, (cnt + 1, nx, ny))
-                        visited[nx][ny] = True
-
-    if not ans:
-        return -1
-    else:
-        return min(ans)
+        if nx < 0 or nx >= n or ny < 0 or ny >= n:
+            continue
+        
+        if arr[nx][ny] == 1:
+            continue
+        
+        if step[nx][ny] == INF:
+            step[nx][ny] = cnt + 1
+            q.append((nx, ny, cnt + 1))
 
 
 result = [[0] * n for _ in range(n)]
@@ -77,7 +59,10 @@ result = [[0] * n for _ in range(n)]
 for i in range(n):
     for j in range(n):
         if arr[i][j] == 2:
-            result[i][j] = check(i, j)
+            if step[i][j] == INF:
+                result[i][j] = -1
+            else: 
+                result[i][j] = step[i][j]
 
 for i in range(n):
     print(' '.join(map(str, result[i])))
