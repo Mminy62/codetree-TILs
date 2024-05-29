a3e090c083229441b978e9ff656d707bed300687
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2024-05-29 12:23:00 +0900

    Title: 특별한 문자, Time: 247ms, Memory: 27MB, Status: Passed - CodeTree

--- 240529/특별한 문자/special-character.py
+++ 240529/특별한 문자/special-character.py
@@ -1,25 +1,15 @@
 from collections import defaultdict
+
 dic = defaultdict(int)
 word = list(input())
-for i, v in enumerate(word):
+
+for v in word:
     dic[v] += 1
 
-# 가장 먼저 등장한걸 출력..
-# 개수를 key, 값을 value, index
-result = []
-for k, v in dic.items():
-    if v == 1:
-        result.append(k)
+ans = "None"
+for c in word:
+    if dic[c] == 1:
+        ans = c
+        break
 
-if not result:
-    print("None")
-
-elif len(result) == 1:
-    print(result[0])
-    
-else:
-    ans = len(word)
-    for c in result:
-        ans = min(ans, word.index(c))
-    
-    print(word[ans])
+print(ans)
