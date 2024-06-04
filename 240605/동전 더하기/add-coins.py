c35088b9f00756dca955f25e6a9f1fb72c0b588f
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2024-06-05 03:12:12 +0900

    Title: 동전 더하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240605/동전 더하기/add-coins.py
@@ -0,0 +1,30 @@
+'''
+배수의 관계이면 가장 큰 걸로 나눠주기 시작하는 것이 좋다
+
+'''
+n, k = map(int, input().split())
+arr = []
+for _ in range(n):
+    arr.append(int(input()))
+
+arr.reverse()
+
+# if result < 0 : break
+def dfs(num, cnt):
+    if num < 0:
+        return -1
+
+    if num == 0:
+        return cnt
+
+    for i in range(n):
+        if num - arr[i] >= 0:
+            return dfs(num - arr[i], cnt + 1)
+
+answer = 10 ** 9
+for i in range(n):
+    temp = dfs(k - arr[i], 1)
+    if temp != -1:
+        answer = min(answer, temp)
+
+print(answer)
