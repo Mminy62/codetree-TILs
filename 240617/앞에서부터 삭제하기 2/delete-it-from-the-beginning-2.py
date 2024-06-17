e4f9e60ef737176b19d23af0f05a792bc6b1122f
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2024-06-17 19:06:45 +0900

    Title: 앞에서부터 삭제하기 2, Time: 5059ms, Memory: 33MB, Status: Time Limit Exceed - CodeTree

--- 240617/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
+++ 240617/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
@@ -1,17 +1,18 @@
 '''
 '''
+from collections import deque
 import heapq
 
 n = int(input())
 arr = list(map(int, input().split()))
-q = []
+q = deque([])
 for i in range(n):
     heapq.heappush(q, arr[i])
 
 res = 0
 for i in range(n - 2):
     item = arr[i]
-    q.remove(item)
+    q.popleft()
     min_num = heapq.heappop(q)
 
     res = max(res, sum(q) / len(q))
