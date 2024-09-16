c4990ff724480ff4e3ed908485795566142b1ab9
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2024-09-16 12:20:42 +0900

    Title: 블럭쌓는 명령, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240916/블럭쌓는 명령/block-stacking-commands.py
+++ 240916/블럭쌓는 명령/block-stacking-commands.py
@@ -1,16 +1,11 @@
-'''
-N개의 칸이 있다. 
-Ai, Bi까지의 각각 블럭을 1씩 쌓으라는 명령이 K번 주어진다. 
-
-'''
-
 N, K = map(int, input().split())
-
-arr = [0] * N
+arr = [0] * (N + 1)
 for _ in range(K):
     a, b = map(int, input().split())
-    for i in range(a - 1, b):
-        arr[i] += 1
+    arr[a] += 1
+    arr[b + 1] -= 1
 
+for i in range(1, N + 1):
+    arr[i] += arr[i - 1]
 arr.sort()
 print(arr[N//2])
