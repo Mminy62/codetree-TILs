71ac2983fcd0e49d7de31100b553bcb0314d9758
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2024-09-21 16:44:44 +0900

    Title: 행복한 수열의 개수, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240921/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240921/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,43 +1,36 @@
 N, M = map(int, input().split())
 
 board = []
+result = 0
 for _ in range(N):
     board.append(list(map(int, input().split())))
 
-# 행끼리 비교
-result = 0
-for i in range(N):
-    cnt = 0
-    pre = 0
-    for j in range(N):
-        if cnt == 0:
-            cnt += 1
-            pre = board[i][j]
-        else:
-            if board[i][j] == pre:
-                cnt += 1
-            if board[i][j] != pre or j == N - 1:
-                if cnt >= M:
-                    result += 1
-                    break
-                cnt = 1
-                pre = board[i][j]
+def search(flag):
+    global result
 
-for i in range(N):
-    cnt = 0
-    pre = 0
-    for j in range(N):
-        if cnt == 0:
-            cnt += 1
-            pre = board[j][i]
-        else:
-            if board[j][i] == pre:
+    for i in range(N):
+        cnt = 1
+        pre = board[i][0] if flag else board[0][i]
+        for j in range(1, N):
+            (r, c) = (i, j) if flag else (j, i)
+            if board[r][c] == pre:
                 cnt += 1
-            if board[j][i] != pre or j == N - 1:
+                if board[r][c] != pre:
+                    if cnt >= M:
+                        result += 1
+                        break
+                    cnt = 1
+                    pre = board[r][c]
+            if j == N - 1:
                 if cnt >= M:
                     result += 1
-                    break
-                cnt = 1
-                pre = board[j][i]
+
+result = 0
+if N == 1 and M == 1:
+    result += N * 2
+
+else: # N > 1
+    search(1)
+    search(0)
 
 print(result)
