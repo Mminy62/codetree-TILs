02e5a63877957d4833e22956a7106302fbaf1422
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2024-09-27 16:17:35 +0900

    Title: 컨베이어 벨트, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240927/컨베이어 벨트/conveyor-belt.py
+++ 240927/컨베이어 벨트/conveyor-belt.py
@@ -7,12 +7,18 @@ belts = []
 for _ in range(2):
     belts += list(map(int, input().split()))
 
-temp = 0
-for _ in range(t):
-    temp = belts[-1]
-    for i in range(n * 2 - 1, 0, -1):
-        belts[i] = belts[i - 1]
-    belts[0] = temp
+# temp = 0
+# for _ in range(t):
+#     temp = belts[-1]
+#     for i in range(n * 2 - 1, 0, -1):
+#         belts[i] = belts[i - 1]
+#     belts[0] = temp
         
+# for i in range(2):
+#     print(" ".join(map(str, belts[i * n: i * n + n])))
+
+
+belts = belts[-t:] + belts[:-t]
+
 for i in range(2):
     print(" ".join(map(str, belts[i * n: i * n + n])))
