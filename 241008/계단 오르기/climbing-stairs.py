82a0b1e8946472615bc7300fe38cd26bb65a8025
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2024-10-08 00:07:07 +0900

    Title: 계단 오르기, Time: 152ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241008/계단 오르기/climbing-stairs.py
+++ 241008/계단 오르기/climbing-stairs.py
@@ -2,9 +2,9 @@ from collections import deque
 
 n = int(input())
 dp = [0] * (n + 1)
+dp[0] = 1
 dp[2] = 1
-if n > 2:
-    dp[3] = 1
+if n > 3:
     for i in range(3, n + 1):
         dp[i] = dp[i - 2] + dp[i - 3]
 
